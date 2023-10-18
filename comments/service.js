def380ede27ac7fa85f7dd7d60304b6a189da0fb
Muhammad Zain <uuu81201@gmail.com>
2023-10-18 11:25:23 +0500

    Errors Resolved

--- comments/service.js
+++ comments/service.js
@@ -1,39 +1,45 @@
-const express = require('express')
-const { randomBytes } = require('crypto')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const axios = require('axios')
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 
-app = express()
+app = express();
 
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
-const commentsByPostId = {}
+const commentsByPostId = {};
 
-app.get('/posts/:id/comments', (req, res) => {
-    const id = req.params.id
-    res.status(200).json({
-        status: 'success',
-        comments: commentsByPostId[id] || []
-    })
-})
+app.get("/posts/:id/comments", (req, res) => {
+  const id = req.params.id;
+  res.status(200).json({
+    comments: commentsByPostId[id] || [],
+  });
+});
 
-app.post('/posts/:id/comments', async (req, res) => {
-    const id = req.params.id
-    const comments = commentsByPostId[id] || []
-    const { content } = req.body
-    const commentId = randomBytes(4).toString('hex')
-    comments.push({ id: commentId, content })
-    commentsByPostId[id] = comments
-    await axios.post('http://localhost:4005/events', { type: 'CommentCreated', data: { id: commentId, content, postId: id } })
-    res.status(201).json({
-        status: 'success',
-        comments
-    })
-})
+app.post("/posts/:id/comments", async (req, res) => {
+  const id = req.params.id;
+  const comments = commentsByPostId[id] || [];
+  const { content } = req.body;
+  const commentId = randomBytes(4).toString("hex");
+  comments.push({ id: commentId, content });
+  commentsByPostId[id] = comments;
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: { id: commentId, content, postId: id },
+  });
+  console.log(comments);
+  res.status(201).json({
+    comments,
+  });
+});
 
+app.post("/events", (req, res) => {
+  console.log("Event : " + req.body.type);
+  res.send({});
+});
 
 app.listen(4001, () => {
-    console.log('Server started : 127.0.0.1:4001')
-})
+  console.log("Server started : 127.0.0.1:4001");
+});
--- posts/service.js
+++ posts/service.js
@@ -1,35 +1,43 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const { randomBytes } = require('crypto')
-const axios = require('axios')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const axios = require("axios");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
-const Posts = {}
+const Posts = {};
 
-app.get('/posts', (req, res) => {
-    res.status(200).json(Posts)
-})
+app.get("/posts", (req, res) => {
+  res.status(200).json({ posts: Posts });
+});
 
-app.post('/posts', async (req, res) => {
-    const id = randomBytes(4).toString('hex')
-    const post = {
-        id,
-        name: req.body
-    }
-    Posts[id] = post
-    await axios.post("http://localhost:4005/events", { type: 'PostCreated', data: post })
-    res.status(201).json({
-        status: 'success',
-        post
-    })
-})
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const post = {
+    id,
+    title: req.body.title,
+  };
+  Posts[id] = post;
 
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: post,
+  });
+
+  res.status(201).json({
+    post,
+  });
+});
+
+app.post("/events", (req, res) => {
+  console.log("Event : " + req.body.type);
+  res.send({});
+});
 
 app.listen(4000, () => {
-    console.log('Server up and running : localhost:4000')
-})
+  console.log("Server up and running : localhost:4000");
+});
