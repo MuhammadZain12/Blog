480bb5a58b731af37def91d52b95322d16ab6c58
Muhammad Zain <uuu81201@gmail.com>
2023-11-12 10:07:41 +0500

    ingress url limitaion handled for /posts, yaml for client

--- posts/service.js
+++ posts/service.js
@@ -15,7 +15,7 @@ app.get("/posts", (req, res) => {
   res.status(200).json({ posts: Posts });
 });
 
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const post = {
     id,
